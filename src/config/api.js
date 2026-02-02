b571280976197be09778e313283aa4219644a672
Elian Muriel <elian_2546@hotmail.com>
2026-02-02 08:19:35 -0500

    Cambio en ip para consumo de api

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://20.171.254.45/api'
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  'https://inmobi-casita.desarrollo-software.xyz/api'
 
 export default API_BASE_URL
